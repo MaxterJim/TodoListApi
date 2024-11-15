76a5245ac89dcde5ae1cc9f3d9cb331978e874eb
MaxterJim <jaime.robledo.villacob@gmail.com>
2024-11-14 23:48:22 -0500

    Agregar archivos de proyecto.

---
+++ TodoListApi/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApi.Models;
+
+namespace TodoListApi.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options){ }
+        public DbSet<TodoItem>todoItems { get; set; } 
+    }
+}
---
+++ TodoListApi/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoListApi.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } // el signo ? es para decirle que acepte nulos null
+        public Boolean? IsComplete { get; set; }
+    }
+}
